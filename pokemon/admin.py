57f4adaf255686cba856c147e0430052b5ba9eb6
apple <apple@apples-MacBook-Pro.local>
2022-07-19 08:52:43 +0300

    i did the taks

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Pokemon
 
-# Register your models here.
-admin.site.register(Pokemon)
+
+
+class PokemonAdmin (admin.ModelAdmin):
+    list_display = ("id","name","hp","active",)
+    list_filter = ("active",)
+    list_display_links = ("id","name",)
+    fieldsets = ((None,{"fields":("name","type","hp","active")}), ("Localizations",{"classes":("collapse",), "fields":("name_fr","name_ar","name_jp"),}),)
+
+
+
+admin.site.register(Pokemon,PokemonAdmin)
